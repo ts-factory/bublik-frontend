0311921bc9a9ac1bf1f9d6ded3aef8f07e8c25aa
Danil Kostromin <danil.kostromin@arknetworks.am>
2024-11-05 13:14:18 +0300

    chore: fixed formatting for build script
    Signed-off-by: Danil Kostromin <danil.kostromin@arknetworks.am>

--- build.sh
+++ build.sh
@@ -11,23 +11,18 @@ echo "------------------------"
 
 rm -r "$CWD"/dist/*
 
-# Get the JSON array from the output
 JSON_ARRAY=$(cat "$CWD"/config.json | jq -c '.[]')
 
 cd "$BUBLIK_UI_LOCATION" || exit
 
-    # Iterate through each item in the JSON array
-    while IFS= read -r ITEM; do
-        # Extract the value of the "base" key
-        BASE_URL=$(echo "$ITEM" | jq -r '.base')
-        # Extract output folder name value
-        OUTPUT_FOLDER=$(echo "$ITEM" | jq -r '.output')
+while IFS= read -r ITEM; do
+  BASE_URL=$(echo "$ITEM" | jq -r '.base')
+  OUTPUT_FOLDER=$(echo "$ITEM" | jq -r '.output')
 
-        echo "#############################################"
-        echo -e "\e[1;34mStarting build for $BASE_URL\e[0m"
-        echo -e "\e[1;33mOutput folder is $OUTPUT_FOLDER\e[0m"
-        echo "#############################################"
+  echo "#############################################"
+  echo -e "\e[1;34mStarting build for $BASE_URL\e[0m"
+  echo -e "\e[1;33mOutput folder is $OUTPUT_FOLDER\e[0m"
+  echo "#############################################"
 
-        # Run build command
-        BASE_URL="$BASE_URL" nx run bublik:build --base="$BASE_URL" --outputPath=../dist/"$OUTPUT_FOLDER" --sourcemap="true"
-    done <<< "$JSON_ARRAY"
+  BASE_URL="$BASE_URL" nx run bublik:build --base="$BASE_URL" --outputPath=../dist/"$OUTPUT_FOLDER" --sourcemap="true"
+done <<<"$JSON_ARRAY"
